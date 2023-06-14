8cadb3df3d50968872009439644a9cefc74fdb5d
Khatchi <solomon9ice2014@gmail.com>
2023-06-14 12:13:45 -0700

    A func. that replaces/adds a key/value in a dic.

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """
+    Replaces or adds key/value to a dictionary.
+    """
+    a_dictionary[key] = value
+
+    return (a_dictionary)
