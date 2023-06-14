1d6b8cdc8fd7688bbb237d4302892a599f69acee
Khatchi <solomon9ice2014@gmail.com>
2023-06-14 11:25:42 -0700

    Replace element of list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,6 +3,6 @@ def search_replace(my_list, search, replace):
     """
     Replace all occurence of an element by another in a new list.
     """
-    new_list = list(map(lambda x: replace if x  == search else x, my_list))
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
 
     return (new_list)
