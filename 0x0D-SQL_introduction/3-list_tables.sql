cf76cf60725eeaedf571a966dde4f28724cb4392
Khatchi <solomon9ice2014@gmail.com>
2023-08-15 09:07:17 -0700

    ...a script that lists all tables MySQL server

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- a script that lists all the tables of a database in your MySQL server.
+-- the database name will be passed as argument of mysql command.
+-- NB: mysql is the name of the database.
+
+SHOW TABLES;
