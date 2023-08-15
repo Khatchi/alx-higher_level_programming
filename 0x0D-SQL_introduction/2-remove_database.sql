01265acc0f4acb30dbd93cf1b3cabdc0735cd125
Khatchi <solomon9ice2014@gmail.com>
2023-08-15 08:55:26 -0700

    ...script that deletes database if exists

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- a scrit that deletes database hbtn_0c_0 from your MySQl server.
+-- script should not fail if hbtn_oc_0 doesn't exist.
+-- do not to use the SELECT or SHOW statements.
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
