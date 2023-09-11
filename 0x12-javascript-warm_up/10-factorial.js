57de6f89f0c3260dfcf2730ea29d9de83c93bd5e
Khatchi <solomon9ice2014@gmail.com>
2023-09-11 16:05:33 -0700

    ...script that computes and prints a factorial: updated

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
 
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+}
+
+console.log(factorial(Number(process.argv[2])));
