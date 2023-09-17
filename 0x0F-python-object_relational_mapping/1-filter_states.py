bcfc941fd3b1e687c704abb3949a803a59cd6821
Khatchi <solomon9ice2014@gmail.com>
2023-09-17 10:40:59 -0700

    ...lists states starting with upper N in a database

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module lists all the states with a name starting
+with N(upper N) in the database hbtn_0e_0_usa.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    connect = MySQLdb.connect(host="localhost", user=argv[1],
+                              port=3306, passwd=argv[2], db=argv[3])
+    """
+    Connect to MySQL database.
+    """
+
+    cursor = connect.cursor()
+    """
+    Creates a cursor object to interact with the database.
+    """
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY
+                   'N%' ORDER BY states.id ASC")
+    """Executes MySQL querry."""
+
+    rows = cursor.fetchall()
+    """Fetches all the selected rows."""
+
+    for row in rows:
+        print(row)
