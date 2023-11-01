a7a4227f66b63a7d4d1899a26ca738411c3687be
Khatchi <solomon9ice2014@gmail.com>
2023-11-01 18:56:36 +0100

    ..No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Find the <header> element using document.querySelector
+  const header = document.querySelector('header');
+
+  // Update the text color to red (#FF0000)
+  header.style.color = '#FF0000';
+});
+
