7359e7830354b4df014e8c91816e7ec3f47c9aba
Khatchi <solomon9ice2014@gmail.com>
2023-11-01 19:06:55 +0100

    ... adds, removes and clears LI elements from a list when the user clicks

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("#add_item").click(function() {
+    // Add a new <li> element to UL.my_list
+    $("ul.my_list").append("<li>Item</li>");
+  });
+
+  $("#remove_item").click(function() {
+    // Remove the last <li> element from UL.my_list
+    $("ul.my_list li:last-child").remove();
+  });
+
+  $("#clear_list").click(function() {
+    // Remove all <li> elements from UL.my_list
+    $("ul.my_list").empty();
+  });
+});
+
