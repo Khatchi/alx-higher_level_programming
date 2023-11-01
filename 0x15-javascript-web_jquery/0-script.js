084e9af964d0740c7abbec69cd88d172c1664dde
Khatchi <solomon9ice2014@gmail.com>
2023-11-01 16:24:28 +0100

    ...updates the text color of the <header> element to red (#FF0000):

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// Find the <header> element using document.querySelector
+const header = document.querySelector('header');
+
+// Update the text color to red (#FF0000) if the element exists
+header.style.color = '#FF0000';
