77f672d35c64d45260c110eccf025b52b0288656
Khatchi <solomon9ice2014@gmail.com>
2023-11-01 17:05:14 +0100

    ...updates the text color of the <header> element to red (#FF0000) using JQuery A

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,10 @@
+// Update the text color of the <header> element to red using jQuery
+$(document).ready(function() {
+  $("header").css("color", "#FF0000");
+});
+
+// Alternatively: ==> shorthand form
+/* $(function() {
+  $("header").css("color", "#FF0000");
+});
+*/
