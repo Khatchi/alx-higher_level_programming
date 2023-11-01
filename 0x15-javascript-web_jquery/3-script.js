7d91081b3662dc438e9db8f2cb9fda1bd6714b1e
Khatchi <solomon9ice2014@gmail.com>
2023-11-01 17:32:26 +0100

    ...adds the class red to the <header> element when the user clicks on the tag DIV#red_header

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  // Attach a click event handler to the DIV#red_header element
+  $("#red_header").click(function() {
+    // Add the "red" class to the <header> element
+    $("header").addClass("red");
+  });
+});
+
